32e56dc27693ae35f548aa17ea592bdec348cc8b
Timothy Batt <jammincoderguy96@gmail.com>
2022-10-01 21:49:45 -0400

    Created API example

--- app.py
+++ app.py
@@ -1,17 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
+import flask
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def display_form():
-    return render_template("form.html")
+
+"""
+These are examples of using Flask as an API 
+from an external app not hosted on the same server.
+"""
+
+@app.route("/api/get")
+def api():
+    # The data you want to send back to the client
+    res_data = {
+        "user": "john",
+        "email": "johndoe@gmail.com",
+        "phone": "+1 234-5678-9012"
+    }
+
+    # Create a response using the your data
+    res = flask.make_response(res_data)
+
+    res.headers["Access-Control-Allow-Origin"] = "*" # This is needed to allow outside requests to access this resource
+
+    # Send the response back to the client
+    return res
 
 
-@app.route("/form", methods=["POST"])
-def handle_form():
-    # Extract username + email from the `form` dictionary of the request
-    username = request.form["username"]
-    email = request.form["email"]
+@app.route("/api/post", methods=["POST"])
+def api_submit():
+    # Extract the "name" value from the posted data
+    name = request.form["name"]
+    
+    # The data to send back to the client
+    res_data = f"OK. Successfully posted name: { name }"
 
-    # Pass in the extracted username + email to the display_info template and return it
-    return render_template("display_info.html", username=username, email=email)
+    # Create the response using your data
+    res = flask.make_response(res_data)
+
+    res.headers["Access-Control-Allow-Origin"] = "*"
+
+    # Send response back to client
+    return res
